41b9bf69da928a204726aa4e57f160d758a77741
unknown <e.roman.v@ya.ru>
2023-02-05 22:44:42 -0300

    small changes in views, model.user, admin

--- api_yamdb/user/models.py
+++ api_yamdb/user/models.py
@@ -1,16 +1,13 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    ADMIN = 'admin'
-    MODERATOR = 'moderator'
-    USER = 'user'
-    ADMIN_ROLE = [
-        (USER, 'user'),
-        (ADMIN, 'admin'),
-        (MODERATOR, 'moderator'),
-    ]
+
+    class Roles(models.TextChoices):
+        ADMIN = 'admin'
+        MODERATOR = 'moderator'
+        USER = 'user'
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ('username',)
@@ -29,31 +26,33 @@ class User(AbstractUser):
     )
     role = models.CharField(
         max_length=9,
-        choices=ADMIN_ROLE,
-        default=USER,
+        choices=Roles.choices,
+        default=Roles.USER,
         verbose_name='Роль пользователя',
         help_text='Укажите роль пользователя',
     )
     first_name = models.CharField(
         verbose_name='Имя пользователя',
         max_length=150,
+        null=True,
     )
     last_name = models.CharField(
         verbose_name='Фамилия пользователя',
         max_length=150,
+        null=True,
     )
 
     class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
 
     @property
     def is_admin(self):
-        return self.is_staff or self.role == self.ADMIN
+        return self.is_staff or self.role == self.Roles.ADMIN
 
     def __str__(self):
         return self.email
 
     @property
     def is_moderator(self):
-        return self.role == self.MODERATOR
+        return self.role == self.Roles.MODERATOR

48ed90df09a167fbd57b78c7e5439fa375eec5c8
Стас AfonyaTriceps <d.vlad957@gmail.com>
2023-02-07 02:35:25 +0300

    commands

--- api_yamdb/reviews/management/commands/load_csv.py
+++ api_yamdb/reviews/management/commands/load_csv.py
@@ -7,32 +7,21 @@ from user.models import User
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):  # noqa: C901
-        del options
-
-        def load_category(**rows):
-            return Category(**rows).save()
-
+    def handle(self, *args, **options):
         for row in csv.DictReader(
             open('./static/data/category.csv', encoding='utf8'),
         ):
-            load_category(id=row['id'], name=row['name'], slug=row['slug'])
-
-        def load_genre(**rows):
-            return Genre(**rows).save()
+            Category(id=row['id'], name=row['name'], slug=row['slug']).save()
 
         for row in csv.DictReader(
             open('./static/data/genre.csv', encoding='utf8'),
         ):
-            load_genre(id=row['id'], name=row['name'], slug=row['slug'])
-
-        def load_user(**rows):
-            return User(**rows).save()
+            Genre(id=row['id'], name=row['name'], slug=row['slug']).save()
 
         for row in csv.DictReader(
             open('./static/data/users.csv', encoding='utf8'),
         ):
-            load_user(
+            User(
                 id=row['id'],
                 username=row['username'],
                 email=row['email'],
@@ -40,46 +29,37 @@ class Command(BaseCommand):
                 bio=row['bio'],
                 first_name=row['first_name'],
                 last_name=row['last_name'],
-            )
-
-        def load_title(**rows):
-            return Title(**rows).save()
+            ).save()
 
         for row in csv.DictReader(
             open('./static/data/titles.csv', encoding='utf8'),
         ):
-            load_title(
+            Title(
                 id=row['id'],
                 name=row['name'],
                 year=row['year'],
                 category_id=row['category'],
-            )
-
-        def load_review(**rows):
-            return Review(**rows).save()
+            ).save()
 
         for row in csv.DictReader(
             open('./static/data/review.csv', encoding='utf8'),
         ):
-            load_review(
+            Review(
                 id=row['id'],
                 title_id=row['title_id'],
                 text=row['text'],
                 author_id=row['author'],
                 score=row['score'],
                 pub_date=row['pub_date'],
-            )
-
-        def load_comment(**rows):
-            return Comment(**rows).save()
+            ).save()
 
         for row in csv.DictReader(
             open('./static/data/comments.csv', encoding='utf8'),
         ):
-            load_comment(
+            Comment(
                 id=row['id'],
                 review_id=row['review_id'],
                 text=row['text'],
                 author_id=row['author'],
                 pub_date=row['pub_date'],
-            )
+            ).save()
